1148a837477d7d0aea6b72c1044c2d9bb02f8acf
Ricardo Augusto de Oliveira Souto <ricardoaos@hotmail.com>
2021-01-18 13:47:15 -0300

    Ajustes na versão do entity

--- Connector.Web/ClientApp/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-import { Host } from "../app/utils/host";
-
-export const environment = {
-    production: true,
-
-    authorityEndPoint: `https://localhost:5000`,
-    applicationEndPoint: Host.getHost(),
-};
--- Connector.Web/ClientApp/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-import { Host } from "../app/utils/host";
-
-export const environment = {
-    production: false,
-
-    authorityEndPoint: `https://localhost:5000`,
-    applicationEndPoint: Host.getHost(),
-};
-
